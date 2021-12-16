553376f19a4801056bcfa0b882c0170ac185fd30
manuelcarreno <73313847+manuelcarreno@users.noreply.github.com>
2021-12-16 19:30:15 -0300

    Update EjemploSeccion2.go

--- EjemploSeccion2.go
+++ EjemploSeccion2.go
@@ -5,41 +5,41 @@ import (
 	"time"
 )
 
-func GorrutinaA(done chan bool) {
+func CorrutinaA(done chan bool) {
 	for i := 1; i < 11; i++ {
 		time.Sleep(1 * time.Second)
 		fmt.Println(i)
 		if i == 3 {
-			go GorrutinaB(done)
+			go CorrutinaB(done)
 			Pausa(&done)
 		}
 		if i == 7 {
-			go GorrutinaC(done)
+			go CorrutinaC(done)
 			Pausa(&done)
 		}
 	}
 	fmt.Println("FIN de la Corrutina A")
 }
 
-func GorrutinaB(done chan bool) {
-	fmt.Println("Pausa de Gorrutina A\nLLamada a Gorrutina B")
+func CorrutinaB(done chan bool) {
+	fmt.Println("Pausa de Corrutina A\nLLamada a Corrutina B")
 	letras := [4]string{"a", "b", "c", "d"}
 	for i := 0; i < 4; i++ {
 		time.Sleep(1 * time.Second)
 		fmt.Println(letras[i])
 	}
-	fmt.Println("Termino de Gorrutina B\nPlay a Gorrutina A")
+	fmt.Println("Termino de Corrutina B\nPlay a Corrutina A")
 	Play(&done)
 }
 
-func GorrutinaC(done chan bool) {
-	fmt.Println("Pausa de Gorrutina A\nLLamada a Gorrutina C")
+func CorrutinaC(done chan bool) {
+	fmt.Println("Pausa de Corrutina A\nLLamada a Corrutina C")
 	colores := [4]string{"azul", "rojo", "amarillo", "verde"}
 	for i := 0; i < 4; i++ {
 		time.Sleep(1 * time.Second)
 		fmt.Println(colores[i])
 	}
-	fmt.Println("Termino de Gorrutina C\nPlay a Gorrutina A")
+	fmt.Println("Termino de Corrutina C\nPlay a Corrutina A")
 	Play(&done)
 }
 
@@ -49,3 +49,4 @@ func Pausa(done *chan bool) {
 func Play(done *chan bool) {
 	*done <- true
 }
+
